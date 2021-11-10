6db71229cb9636cb3912d2fb30b593ad80bc7da2
Habuwny <habuwny@gmail.com>
2021-11-11 01:09:19 +0200

    add auth sets for login and logout

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Ask </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+    &copy; 2021 <a href="http://habuwny.dev">Habuwny</a>. All Rights Reserved.
+</footer>
